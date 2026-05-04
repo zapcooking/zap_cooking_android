a55ebdfee67a9f77dbb04bdeb1a84fa4ebd630ea
Barry Deen <barrydeen@protonmail.com>
2026-05-04 08:33:22 -0400

    chore: bump version to 1.0.4 (78)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = baseApplicationId
         minSdk = 26
         targetSdk = 35
-        versionCode = 77
-        versionName = "1.0.3"
+        versionCode = 78
+        versionName = "1.0.4"
         resValue("string", "app_name", "Wisp")
 
         ndk {
